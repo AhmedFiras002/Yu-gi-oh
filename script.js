5874463c184a9e5466dca78ebb9fd1e7513d1007
AhmedFiras002 <firassport123@gmail.com>
2023-08-03 03:56:58 +0200

    getting the cards and their ratings

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 "use strict"
 document.addEventListener('DOMContentLoaded',init);
-function init(){
-    console.log('hi');
-    getCards()
+let type;
+let randomNumber = Math.floor(Math.random() * 7774);
+ function init(){
+    document.querySelector("#draw").addEventListener('click',function(){
+        getCards()
+        
+    } );
 }
+
+function nav(){}
 function getCards(){
     fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php')
     .then(resp=>resp.json())
     .then(json=>renderCards(json))
+    .catch(error => console.error(error));
 }
-function renderCards(json){
-    console.log(json);
+function renderCards(json){console.log();
+   let filterCardsType = json.data.filter(x=>x.type.includes('Monster'));
+   let filterCardsRating = filterCardsType.filter(y=>y.atk || y.def > 0);
+   let picture = filterCardsRating[randomNumber].card_images[0].image_url; 
+        let container = document.querySelector('#container');
+        let img = `<img src="${picture}" alt="">`
+       container.insertAdjacentHTML('beforeend',img)
+       document.querySelector("#attacking").addEventListener('click',function(){
+       
+       })
+       document.querySelector("#defending").addEventListener('click',function(){
+         return json.data[randomNumber].def;
+       })
 }
